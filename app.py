f9f338cdbacc8df30cf89975e83f40c820d65f3c
aviyall <aviyall.git@gmail.com>
2025-01-12 14:40:37 +0530

    Update app.py

--- app.py
+++ app.py
@@ -118,7 +118,7 @@ def result():
         SELECT "BK_NAME", "BK_ID", "BOOK_STATUS", "AUTHOR_NAME"
         FROM library
         WHERE similarity("BK_NAME", %s) > 0.3
-           OR "BOOK_NAME" ILIKE %s;
+           OR "BK_NAME" ILIKE %s;
         """
         cursor.execute(query, (f"{search_input_upper}",f"%{search_input_upper}%"))
         books = cursor.fetchall()
